315ffed031ca45475a02e01a7865f5d44d8aa706
zyad2012104-maker <zyad2012104@gmail.com>
2026-03-30 07:12:56 +0200

    Update JSONBIN BIN_ID in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const CONFIG = {
     JSONBIN: {
-        BIN_ID: '69c13f81b7ec241ddc956318',
+        BIN_ID: '69c9835736566621a85cb01f',
         MASTER_KEY: '$2a$10$5X1fbgOhCiGV23rKGUkLLuhD/a1eIHNuKwvtNzKwu3W7KT8CGpaG.',
         BASE_URL: 'https://api.jsonbin.io/v3/b/'
     }
